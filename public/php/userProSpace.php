a726b8e48f7c3974f6ed990ce7a72e672a1f07ec
Raymond <raymond.ballo.networking@gmail.com>
2022-07-02 20:10:58 +0200

    [Projet Web] connexion to page users done

---
+++ public/php/userProSpace.php
@@ -0,0 +1,31 @@
+<?php
+  session_start();
+  require_once 'config.php'; // ajout connexion bcadd
+  // si la session existe pas soit si l'on est pas connecté on redirige
+
+  if(!isset($_SESSION['user'])){
+    header('Location:index.php');
+    die();
+  }
+
+  // On récupère les données de l'utilisateur
+
+  $req = $bdd->prepare('SELECT pseudo,token FROM UserAccount WHERE token = ?');
+  $req->execute(array($_SESSION['user']));
+  $data = $req->fetch();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Espace Professionnel</title>
+    <link rel="stylesheet" href="../css/styleUserClient.css"/>
+  </head>
+  <body>
+    <div class="container">
+      <h2> Bienvenue sur l'espace Professionnel <?php if(is_array($data))echo $data['pseudo']; ?> !</h2>
+      <a href="deconnexion.php" class="btn"> Déconnexion </a>
+    </div>
+  </body>
+</html>
